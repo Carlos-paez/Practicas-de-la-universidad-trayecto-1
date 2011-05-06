a0239c216bb21d8667253cff0cd5247f4807ee82
Carlos-paez <carlopaezguerra@gmail.com>
2011-05-06 00:18:23 -0430

    Segunda practica en clase u do-whilw en la factura

--- Factura de compra/main/Program.cs
+++ Factura de compra/main/Program.cs
@@ -9,47 +9,49 @@ class Program
         double total_pagar;
         DateTime fecha = DateTime.Now;
         
-	        while (true){
+	        
 	
 	        Console.WriteLine("Ingrese la acción que desea realizar: \n    1 = crear factura. \n    2 = Cierre de caja.");
 	        int opccion = int.Parse(Console.ReadLine());
 	
 	        Console.Clear();
 	
-	        if (opccion == 1)
+	        do 
 	        {
 	           
 	                // Pedir y capturar datos de los productos
-	                Console.WriteLine("Ingrese el nombre del primer producto, presione enter y luego el precio del mismo:");
-	                string producto1 = Console.ReadLine();
-	                double precio1 = double.Parse(Console.ReadLine());
+		                Console.WriteLine("Ingrese el nombre del primer producto, presione enter y luego el precio del mismo:");
+		                string producto1 = Console.ReadLine();
+		                double precio1 = double.Parse(Console.ReadLine());
+		
+		                Console.WriteLine("Ingrese el nombre del segundo producto, presione enter y luego el precio del mismo:");
+		                string producto2 = Console.ReadLine();
+		                double precio2 = double.Parse(Console.ReadLine());
+		
+		                Console.WriteLine("Ingrese el nombre del tercer producto, presione enter y luego el precio del mismo:");
+		                string producto3 = Console.ReadLine();
+		                double precio3 = double.Parse(Console.ReadLine());
+		                
+		
+		                // Calcular el IVA de cada producto
+		                double iva1 = precio1 * 0.16;
+		                double iva2 = precio2 * 0.16;
+		                double iva3 = precio3 * 0.16;
+		
+		                // Capturar datos del cliente
+		                Console.WriteLine("Ingrese el nombre y apellido del cliente");
+		                string nombre_cliente = Console.ReadLine();
+		
+		                Console.WriteLine("Ingrese la dirección actual del cliente");
+		                string direccion = Console.ReadLine();
+	                
 	
-	                Console.WriteLine("Ingrese el nombre del segundo producto, presione enter y luego el precio del mismo:");
-	                string producto2 = Console.ReadLine();
-	                double precio2 = double.Parse(Console.ReadLine());
-	
-	                Console.WriteLine("Ingrese el nombre del tercer producto, presione enter y luego el precio del mismo:");
-	                string producto3 = Console.ReadLine();
-	                double precio3 = double.Parse(Console.ReadLine());
-	
-	                // Calcular el IVA de cada producto
-	                double iva1 = precio1 * 0.16;
-	                double iva2 = precio2 * 0.16;
-	                double iva3 = precio3 * 0.16;
-	
-	                // Capturar datos del cliente
-	                Console.WriteLine("Ingrese el nombre y apellido del cliente");
-	                string nombre_cliente = Console.ReadLine();
-	
-	                Console.WriteLine("Ingrese la dirección actual del cliente");
-	                string direccion = Console.ReadLine();
-	
-	                Console.WriteLine("\n\n\n");
-	
-	                double total_base = precio1 + precio2 + precio3;
-	                total_pagar = total_base + iva1 + iva2 + iva3; // Total a pagar incluye el IVA
-	                total_dia += total_pagar; // Acumular total del día
-	                factura_numero++; // Incrementar el número de factura
+		                	
+		                double total_base = precio1 + precio2 + precio3;
+		                total_pagar = total_base + iva1 + iva2 + iva3; // Total a pagar incluye el IVA
+		                total_dia += total_pagar; // Acumular total del día
+		                factura_numero++; // Incrementar el número de factura
+		               
 	
 	                Console.Clear();
 	
@@ -92,8 +94,7 @@ class Program
 	                }
 	                Console.ReadKey(true);
 	                Console.Clear();
-	            }
-	        
+	                
 	
 	        if (opccion == 2)
 	        {
@@ -102,6 +103,7 @@ class Program
 	            Console.ReadKey(true);
 	            Console.Clear();
 	        }
+	    while(opccion == 1)
         }
     }
 }
---
+++ Practica 2 (bucle do-while y condicionales)/Practica 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Practica_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			// Uso de If, Else...
+			
+			/*operadores de comparación: ==, >, <, diferente
+			  Operadores Aricmeticos: exonentes, * - + /   
+			  Operadores logicos: and, or, not 
+			 */
+			
+			
+			int x;
+			
+			Console.WriteLine("Algoritmo que calcula expresiones \nIntodusca el numero a calcular");
+			x = int.Parse(Console.ReadLine()); 
+			
+			x = x^2+2*x+1;
+			
+			Console.WriteLine("El resultado es: " + x);
+			
+			if (x <= 80)
+			{
+				Console.WriteLine("El resultado es menor o igual a 80");
+			}
+			
+			else
+			{
+				Console.WriteLine("El resultado es mayor a 80");
+			}
+			
+			
+			Console.ReadKey(true);
+			Console.Clear();
+			
+			
+			do
+			{
+				Console.WriteLine("el numeor es mayor a 100");
+			}while(x<=100);
+			
+			Console.ReadKey(true);
+			
+		}
+	}
+}
