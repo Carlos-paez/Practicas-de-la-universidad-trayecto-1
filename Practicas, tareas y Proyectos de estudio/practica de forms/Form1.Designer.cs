58a23b94f706ac8c7efb5a49ca56b424aaed7ade
Carlos-paez <carlospaezguerra@gmail.com>
2011-05-06 00:07:46 -0430

    Practica en clase de windows forms

--- Practicas, tareas y Proyectos de estudio/practica de forms/Form1.Designer.cs
+++ Practicas, tareas y Proyectos de estudio/practica de forms/Form1.Designer.cs
@@ -31,19 +31,19 @@ namespace practica_de_forms
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.name = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.password = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -59,16 +59,17 @@ namespace practica_de_forms
             this.panel1.Size = new System.Drawing.Size(665, 52);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // label1
             // 
-            this.panel2.BackColor = System.Drawing.Color.MediumTurquoise;
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Location = new System.Drawing.Point(1, 52);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(66, 322);
-            this.panel2.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("FiraCode Nerd Font Mono Light", 30F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(224, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 49);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inforteg ";
             // 
             // button1
             // 
@@ -81,27 +82,16 @@ namespace practica_de_forms
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // panel2
             // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Location = new System.Drawing.Point(11, 35);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(44, 38);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Location = new System.Drawing.Point(11, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(42, 37);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.panel2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(1, 52);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(66, 322);
+            this.panel2.TabIndex = 1;
             // 
             // button4
             // 
@@ -114,17 +104,27 @@ namespace practica_de_forms
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label1
+            // button3
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Font = new System.Drawing.Font("FiraCode Nerd Font Mono Light", 30F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(224, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(247, 49);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Inforteg ";
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(11, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 37);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(11, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 38);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel3
             // 
@@ -140,24 +140,26 @@ namespace practica_de_forms
             this.panel3.Size = new System.Drawing.Size(285, 284);
             this.panel3.TabIndex = 2;
             // 
-            // label2
+            // button6
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(97, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "User or Email";
+            this.button6.Location = new System.Drawing.Point(13, 246);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 25);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Cancelar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // name
+            // button5
             // 
-            this.name.Location = new System.Drawing.Point(63, 45);
-            this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(167, 20);
-            this.name.TabIndex = 1;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(230, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 37);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // password
             // 
@@ -178,26 +180,24 @@ namespace practica_de_forms
             this.label3.TabIndex = 2;
             this.label3.Text = "Password";
             // 
-            // button5
+            // name
             // 
-            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button5.Location = new System.Drawing.Point(230, 234);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(43, 37);
-            this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.name.Location = new System.Drawing.Point(63, 45);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(167, 20);
+            this.name.TabIndex = 1;
             // 
-            // button6
+            // label2
             // 
-            this.button6.Location = new System.Drawing.Point(13, 246);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(89, 25);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Cancelar";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(97, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "User or Email";
             // 
             // Form1
             // 
--- Practicas, tareas y Proyectos de estudio/practica de forms/Form6.Designer.cs
+++ Practicas, tareas y Proyectos de estudio/practica de forms/Form6.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace practica_de_forms
 {
-    partial class Form3
+    partial class Form6
     {
         /// <summary>
         /// Required designer variable.
@@ -33,10 +33,8 @@ namespace practica_de_forms
             this.panel4 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.fecha = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel4.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -44,7 +42,7 @@ namespace practica_de_forms
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.MediumTurquoise;
-            this.panel3.Location = new System.Drawing.Point(2, 53);
+            this.panel3.Location = new System.Drawing.Point(1, 53);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(66, 322);
             this.panel3.TabIndex = 7;
@@ -53,7 +51,7 @@ namespace practica_de_forms
             // 
             this.panel4.BackColor = System.Drawing.Color.DarkTurquoise;
             this.panel4.Controls.Add(this.label2);
-            this.panel4.Location = new System.Drawing.Point(2, 2);
+            this.panel4.Location = new System.Drawing.Point(1, 2);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(665, 52);
             this.panel4.TabIndex = 6;
@@ -74,70 +72,45 @@ namespace practica_de_forms
             // 
             this.panel1.BackColor = System.Drawing.Color.DimGray;
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(171, 66);
+            this.panel1.Controls.Add(this.fecha);
+            this.panel1.Location = new System.Drawing.Point(73, 69);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(355, 297);
+            this.panel1.Size = new System.Drawing.Size(582, 295);
             this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // fecha
+            // 
+            this.fecha.AutoSize = true;
+            this.fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fecha.ForeColor = System.Drawing.Color.White;
+            this.fecha.Location = new System.Drawing.Point(64, 60);
+            this.fecha.Name = "fecha";
+            this.fecha.Size = new System.Drawing.Size(22, 31);
+            this.fecha.TabIndex = 0;
+            this.fecha.Text = ".";
+            this.fecha.Click += new System.EventHandler(this.fecha_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(125, 245);
+            this.button1.Location = new System.Drawing.Point(255, 255);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 25);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Cerrar";
+            this.button1.Size = new System.Drawing.Size(82, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.CadetBlue;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(52, 180);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(233, 20);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Menu Iconocgrafico de botones";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.CadetBlue;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(21, 95);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(319, 20);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Interacción de información entre formularios";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.CadetBlue;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(32, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Esta aplicación cuneta con 5 formularios";
-            // 
-            // Form3
+            // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 375);
+            this.ClientSize = new System.Drawing.Size(667, 376);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel4);
-            this.Name = "Form3";
-            this.Text = "Form3";
+            this.Name = "Form6";
+            this.Text = "Form6";
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -152,9 +125,7 @@ namespace practica_de_forms
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label fecha;
         private System.Windows.Forms.Button button1;
     }
 }
